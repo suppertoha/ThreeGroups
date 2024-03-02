a035a4f85299c8e0ff2e80731d9872199cd64dcb
unknown <suppertoha@yandex.ru>
2024-03-02 20:20:48 +0300

    select and dropdown

--- app/js/main.js
+++ app/js/main.js
@@ -175,32 +175,99 @@ __webpack_require__.r(__webpack_exports__);
   \****************************************/
 /***/ (() => {
 
-document.querySelectorAll(".accordion-start-js").forEach(el => {
-  el.addEventListener("click", () => {
-    const elems = document.querySelectorAll(".accordion-start-js");
-    elems.forEach(elem => {
-      if (elem !== el) {
-        elem.classList.remove("open");
-        elem.nextElementSibling.classList.remove("open");
-      }
-    });
-    el.classList.toggle("open");
-    let content = el.nextElementSibling;
-    if (content.classList.contains("open")) {
-      document.querySelectorAll(".accordion-content-js").forEach(el => {
-        if (content && content.classList.contains("open")) {
-          content.classList.remove("open");
+function accordion() {
+  const accordionJs = document.querySelector(".accordion");
+
+  //document.querySelectorAll(".accordion-start-js").forEach((el) => {
+  //  el.addEventListener("click", () => {
+  //    const elems = document.querySelectorAll(".accordion-start-js");
+  //    elems.forEach((elem) => {
+  //      if (elem !== el) {
+  //        setTimeout(() => {
+  //          elem.classList.remove("open");
+  //          elem.nextElementSibling.classList.remove("open");
+  //        }, 200);
+
+  //      }
+  //    });
+
+  //    el.classList.toggle("open");
+  //    const content = el.nextElementSibling;
+  //    if (content) {
+  //      content.classList.toggle("open");
+  //    }
+  //  });
+  //});
+
+  let faqs = document.querySelectorAll(".faq");
+  if (faqs.length > 0) {
+    faqs.forEach(faq => {
+      faq.addEventListener("click", function (e) {
+        if (e.target.classList.contains("ask")) {
+          let answer = e.target.nextElementSibling;
+          toggleItem(answer, e.target);
         }
+      });
+    });
+    window.addEventListener("click", e => {
+      faqs.forEach(faq => {
+        if (!faq.contains(e.target)) {
+          faq.querySelectorAll(".answer.open").forEach(answer => {
+            jsHeightAnimation(answer, true, function () {
+              answer.classList.remove("open");
+            });
+          });
+        }
+      });
+    });
+  }
+  function toggleItem(answer, ask) {
+    if (answer.classList.contains("open")) {
+      jsHeightAnimation(answer, true, function () {
+        answer.classList.remove("open");
+        ask.classList.remove("open");
       });
     } else {
-      document.querySelectorAll(".accordion-content-js").forEach(el => {
-        if (content && !content.classList.contains("open")) {
-          content.classList.add("open");
+      closeAllAnswers(ask);
+      answer.classList.add("open");
+      ask.classList.add("open");
+      jsHeightAnimation(answer);
+    }
+  }
+  function closeAllAnswers(currentAsk) {
+    faqs.forEach(faq => {
+      faq.querySelectorAll(".answer.open").forEach(answer => {
+        if (answer.previousElementSibling !== currentAsk) {
+          jsHeightAnimation(answer, true, function () {
+            answer.classList.remove("open");
+            answer.previousElementSibling.classList.remove("open");
+          });
         }
       });
+    });
+  }
+  function jsHeightAnimation(el) {
+    let isReverse = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : false;
+    let onFinish = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : function () {};
+    if (el.jsAnimated === true) {
+      return;
     }
-  });
-});
+    el.jsAnimated = true;
+    let animate = el.animate([{
+      height: 0
+    }, {
+      height: el.scrollHeight + "px"
+    }], {
+      duration: 500,
+      direction: isReverse ? "reverse" : "normal"
+    });
+    animate.addEventListener("finish", function () {
+      el.jsAnimated = false;
+      onFinish();
+    });
+  }
+}
+accordion();
 
 /***/ }),
 
@@ -211,138 +278,46 @@ document.querySelectorAll(".accordion-start-js").forEach(el => {
 /***/ (() => {
 
 function hideExcessButtons() {
-  function updateButtons() {
-    const cardContents = document.querySelectorAll('.card-concert__content');
-    cardContents.forEach(content => {
-      const containerHeight = content.offsetHeight;
-      const title = content.querySelector('.card-concert__title');
-      const buttons = content.querySelectorAll('.buttons-banner__item');
-      const titleHeight = title.offsetHeight;
-      //const buttonsBannerHeight = content.querySelector('.buttons-banner').offsetHeight;
-
-      let currentHeight = titleHeight;
-      let excessButtons = 0;
-      let itemMoreButtonHeight = 0;
-      const itemMoreButtonsContainer = content.querySelector('.item-more__buttons');
-      const itemMoreButton = content.querySelector('.item-more');
-      const itemMoreNum = content.querySelector('.item-more__num');
-      buttons.forEach(button => {
-        if (button.classList.contains('item-more')) {
-          itemMoreButtonHeight = button.offsetHeight;
-          button.addEventListener('click', () => {
-            const itemMoreInner = content.querySelector('.item-more__inner');
-            itemMoreInner.classList.toggle('active');
-            checkItemMoreInner(itemMoreInner);
-          });
-          const itemMoreClose = button.querySelector('.item-more__close');
-          itemMoreClose.addEventListener('click', event => {
-            event.stopPropagation();
-            const itemMoreInner = content.querySelector('.item-more__inner');
-            itemMoreInner.classList.remove('active');
-            checkItemMoreInner(itemMoreInner);
-          });
-          return;
-        }
-        currentHeight += button.offsetHeight;
-        if (currentHeight > containerHeight - itemMoreButtonHeight) {
-          button.classList.add('hidden-button');
-          excessButtons++;
-          itemMoreButtonsContainer.appendChild(button);
-        }
-      });
-      if (excessButtons > 0) {
-        itemMoreButton.classList.remove('hidden-button');
-        itemMoreNum.textContent = excessButtons;
-      } else {
-        itemMoreButton.classList.add('hidden-button');
-      }
-      function checkItemMoreInner(itemMoreInner) {
-        const isEmpty = !itemMoreInner.querySelector('*');
-        if (isEmpty && excessButtons === 0) {
-          itemMoreButton.classList.add('hidden-button');
-        } else {
-          itemMoreButton.classList.remove('hidden-button');
-        }
+  const cardContents = document.querySelectorAll(".js-cart");
+  if (cardContents.length === 0) return;
+  cardContents.forEach(content => {
+    const containerHeight = content.clientHeight - parseInt(window.getComputedStyle(content).paddingTop);
+    const title = content.querySelector(".card-concert__title");
+    const titleHeight = title.offsetHeight + parseInt(window.getComputedStyle(title).marginBottom);
+    let currentHeight = titleHeight;
+    let excessButtons = 0;
+    const itemMoreButtonsContainer = content.querySelector(".item-more__buttons");
+    const itemMoreButton = content.querySelector(".item-more");
+    const itemMoreNum = content.querySelector(".item-more__num");
+    const buttons = content.querySelectorAll(".buttons-banner__item:not(.item-more)");
+    buttons.forEach(button => {
+      currentHeight += button.offsetHeight + 5;
+      console.log('currentHeight', currentHeight);
+      if (currentHeight > containerHeight) {
+        button.classList.add("hidden-button");
+        excessButtons++;
+        itemMoreButtonsContainer.appendChild(button.cloneNode(true));
+        button.remove();
       }
     });
-  }
-  window.addEventListener('resize', updateButtons);
-  updateButtons();
+    if (excessButtons > 0) {
+      itemMoreButton.classList.remove("hidden-button");
+      itemMoreNum.textContent = excessButtons;
+    } else {
+      itemMoreButton.classList.add("hidden-button");
+    }
+    itemMoreButton.addEventListener("click", function () {
+      const itemMoreInner = content.querySelector(".item-more__inner");
+      itemMoreInner.classList.toggle("active");
+    });
+  });
 }
-
-//hideExcessButtons();
-
-//function hideExcessButtons() {
-//  function updateButtons() {
-//    const cardContents = document.querySelectorAll('.card-concert__content');
-
-//    cardContents.forEach(content => {
-//      const containerHeight = content.offsetHeight;
-//      const title = content.querySelector('.card-concert__title');
-//      const buttons = content.querySelectorAll('.buttons-banner__item');
-//      const titleHeight = title.offsetHeight;
-//      const buttonsBanner = content.querySelector('.buttons-banner');
-//      const buttonsBannerHeight = buttonsBanner.offsetHeight;
-//      const itemMoreButton = content.querySelector('.item-more');
-//      const itemMoreNum = content.querySelector('.item-more__num');
-
-//      let currentHeight = titleHeight;
-//      let excessButtons = 0;
-//      let itemMoreButtonHeight = 0;
-//      const itemMoreButtonsContainer = content.querySelector('.item-more__buttons');
-
-//      buttons.forEach(button => {
-//        if (button.classList.contains('item-more')) {
-//          itemMoreButtonHeight = button.offsetHeight;
-//          button.addEventListener('click', () => {
-//            const itemMoreInner = content.querySelector('.item-more__inner');
-//            itemMoreInner.classList.toggle('active');
-//            checkItemMoreInner(itemMoreInner);
-//          });
-
-//          const itemMoreClose = button.querySelector('.item-more__close');
-//          itemMoreClose.addEventListener('click', (event) => {
-//            event.stopPropagation();
-//            const itemMoreInner = content.querySelector('.item-more__inner');
-//            itemMoreInner.classList.remove('active');
-//            checkItemMoreInner(itemMoreInner);
-//          });
-
-//          return;
-//        }
-
-//        currentHeight += button.offsetHeight;
-//        console.log(buttonsBannerHeight)
-//        if (buttonsBannerHeight > 0) {
-//          button.classList.add('hidden-button');
-//          excessButtons++;
-//          itemMoreButtonsContainer.appendChild(button);
-//        }
-//      });
-
-//      if (excessButtons > 0) {
-//        itemMoreButton.classList.remove('hidden-button');
-//        itemMoreNum.textContent = excessButtons;
-//      } else {
-//        itemMoreButton.classList.add('hidden-button');
-//      }
-
-//      function checkItemMoreInner(itemMoreInner) {
-//        const isEmpty = !itemMoreInner.querySelector('*');
-//        if (isEmpty && excessButtons === 0) {
-//          itemMoreButton.classList.add('hidden-button');
-//        } else {
-//          itemMoreButton.classList.remove('hidden-button');
-//        }
-//      }
-//    });
-//  }
-
-//  window.addEventListener('resize', updateButtons);
-//  updateButtons();
-//}
-
-//hideExcessButtons();
+hideExcessButtons();
+let resizeTimeout;
+window.addEventListener("resize", function () {
+  clearTimeout(resizeTimeout);
+  resizeTimeout = setTimeout(hideExcessButtons, 100);
+});
 
 /***/ }),
 
--- src/js/components/blockHeightOfTitle.js
+++ src/js/components/blockHeightOfTitle.js
@@ -1,143 +1,49 @@
 function hideExcessButtons() {
-  function updateButtons() {
-    const cardContents = document.querySelectorAll('.card-concert__content');
+  const cardContents = document.querySelectorAll(".js-cart");
+  if (cardContents.length === 0) return;
 
-    cardContents.forEach(content => {
-      const containerHeight = content.offsetHeight;
-      const title = content.querySelector('.card-concert__title');
-      const buttons = content.querySelectorAll('.buttons-banner__item');
-      const titleHeight = title.offsetHeight;
-      //const buttonsBannerHeight = content.querySelector('.buttons-banner').offsetHeight;
+  cardContents.forEach((content) => {
+    const containerHeight = content.clientHeight - parseInt(window.getComputedStyle(content).paddingTop);
+    const title = content.querySelector(".card-concert__title");
+    const titleHeight = title.offsetHeight + parseInt(window.getComputedStyle(title).marginBottom);
+    let currentHeight = titleHeight;
+    let excessButtons = 0;
+    const itemMoreButtonsContainer = content.querySelector(".item-more__buttons");
+    const itemMoreButton = content.querySelector(".item-more");
+    const itemMoreNum = content.querySelector(".item-more__num");
 
-      let currentHeight = titleHeight;
-      let excessButtons = 0;
-      let itemMoreButtonHeight = 0;
-      const itemMoreButtonsContainer = content.querySelector('.item-more__buttons');
-      const itemMoreButton = content.querySelector('.item-more');
-      const itemMoreNum = content.querySelector('.item-more__num');
+    const buttons = content.querySelectorAll(".buttons-banner__item:not(.item-more)");
 
-      buttons.forEach(button => {
-        if (button.classList.contains('item-more')) {
-          itemMoreButtonHeight = button.offsetHeight;
-          button.addEventListener('click', () => {
-            const itemMoreInner = content.querySelector('.item-more__inner');
-            itemMoreInner.classList.toggle('active');
-            checkItemMoreInner(itemMoreInner);
-          });
+    buttons.forEach((button) => {
+      currentHeight += button.offsetHeight + 5;
+      console.log('currentHeight', currentHeight)
 
-          const itemMoreClose = button.querySelector('.item-more__close');
-          itemMoreClose.addEventListener('click', (event) => {
-            event.stopPropagation();
-            const itemMoreInner = content.querySelector('.item-more__inner');
-            itemMoreInner.classList.remove('active');
-            checkItemMoreInner(itemMoreInner);
-          });
-
-          return;
-        }
-
-        currentHeight += button.offsetHeight;
-        if (currentHeight > containerHeight - itemMoreButtonHeight) {
-          button.classList.add('hidden-button');
-          excessButtons++;
-          itemMoreButtonsContainer.appendChild(button);
-        }
-      });
-
-      if (excessButtons > 0) {
-        itemMoreButton.classList.remove('hidden-button');
-        itemMoreNum.textContent = excessButtons;
-      } else {
-        itemMoreButton.classList.add('hidden-button');
-      }
-
-      function checkItemMoreInner(itemMoreInner) {
-        const isEmpty = !itemMoreInner.querySelector('*');
-        if (isEmpty && excessButtons === 0) {
-          itemMoreButton.classList.add('hidden-button');
-        } else {
-          itemMoreButton.classList.remove('hidden-button');
-        }
+      if (currentHeight > containerHeight) {
+        button.classList.add("hidden-button");
+        excessButtons++;
+        itemMoreButtonsContainer.appendChild(button.cloneNode(true));
+        button.remove();
       }
     });
-  }
 
-  window.addEventListener('resize', updateButtons);
-  updateButtons();
+    if (excessButtons > 0) {
+      itemMoreButton.classList.remove("hidden-button");
+      itemMoreNum.textContent = excessButtons;
+    } else {
+      itemMoreButton.classList.add("hidden-button");
+    }
+
+    itemMoreButton.addEventListener("click", function () {
+      const itemMoreInner = content.querySelector(".item-more__inner");
+      itemMoreInner.classList.toggle("active");
+    });
+  });
 }
 
-//hideExcessButtons();
+hideExcessButtons();
 
-//function hideExcessButtons() {
-//  function updateButtons() {
-//    const cardContents = document.querySelectorAll('.card-concert__content');
-
-//    cardContents.forEach(content => {
-//      const containerHeight = content.offsetHeight;
-//      const title = content.querySelector('.card-concert__title');
-//      const buttons = content.querySelectorAll('.buttons-banner__item');
-//      const titleHeight = title.offsetHeight;
-//      const buttonsBanner = content.querySelector('.buttons-banner');
-//      const buttonsBannerHeight = buttonsBanner.offsetHeight;
-//      const itemMoreButton = content.querySelector('.item-more');
-//      const itemMoreNum = content.querySelector('.item-more__num');
-
-//      let currentHeight = titleHeight;
-//      let excessButtons = 0;
-//      let itemMoreButtonHeight = 0;
-//      const itemMoreButtonsContainer = content.querySelector('.item-more__buttons');
-
-//      buttons.forEach(button => {
-//        if (button.classList.contains('item-more')) {
-//          itemMoreButtonHeight = button.offsetHeight;
-//          button.addEventListener('click', () => {
-//            const itemMoreInner = content.querySelector('.item-more__inner');
-//            itemMoreInner.classList.toggle('active');
-//            checkItemMoreInner(itemMoreInner);
-//          });
-
-//          const itemMoreClose = button.querySelector('.item-more__close');
-//          itemMoreClose.addEventListener('click', (event) => {
-//            event.stopPropagation();
-//            const itemMoreInner = content.querySelector('.item-more__inner');
-//            itemMoreInner.classList.remove('active');
-//            checkItemMoreInner(itemMoreInner);
-//          });
-
-//          return;
-//        }
-
-//        currentHeight += button.offsetHeight;
-//        console.log(buttonsBannerHeight)
-//        if (buttonsBannerHeight > 0) {
-//          button.classList.add('hidden-button');
-//          excessButtons++;
-//          itemMoreButtonsContainer.appendChild(button);
-//        }
-//      });
-
-//      if (excessButtons > 0) {
-//        itemMoreButton.classList.remove('hidden-button');
-//        itemMoreNum.textContent = excessButtons;
-//      } else {
-//        itemMoreButton.classList.add('hidden-button');
-//      }
-
-//      function checkItemMoreInner(itemMoreInner) {
-//        const isEmpty = !itemMoreInner.querySelector('*');
-//        if (isEmpty && excessButtons === 0) {
-//          itemMoreButton.classList.add('hidden-button');
-//        } else {
-//          itemMoreButton.classList.remove('hidden-button');
-//        }
-//      }
-//    });
-//  }
-
-//  window.addEventListener('resize', updateButtons);
-//  updateButtons();
-//}
-
-//hideExcessButtons();
-
-
+let resizeTimeout;
+window.addEventListener("resize", function () {
+  clearTimeout(resizeTimeout);
+  resizeTimeout = setTimeout(hideExcessButtons, 100);
+});
