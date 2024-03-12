693f049cdb3942f6e0e7b9f1ab63ad202c2669cb
unknown <suppertoha@yandex.ru>
2024-03-12 16:18:43 +0300

    testing site

--- app/js/main.js
+++ app/js/main.js
@@ -339,6 +339,10 @@ function mainBlockHeight() {
   const itemMoreNum = bannerBlockJs.querySelector(".link-info__date--num");
   const itemMoreButton = bannerBlockJs.querySelector(".item-more");
   let excessButtons = 0;
+  let size = 182;
+  if (document.documentElement.clientWidth < 991) {
+    size = 80;
+  }
   const buttons = bannerBlockJs.querySelectorAll(".buttons-banner__item:not(.item-more)");
   buttons.forEach(button => {
     button.classList.remove("hidden-button");
@@ -346,7 +350,7 @@ function mainBlockHeight() {
   itemMoreButton.classList.add("hidden-button");
   [...buttons].reverse().forEach(button => {
     const buttonsBannerHeight = bannerBlockJs.offsetHeight;
-    if (buttonsBannerHeight > 182) {
+    if (buttonsBannerHeight > size) {
       button.classList.add("hidden-button");
       excessButtons++;
     }
@@ -355,7 +359,7 @@ function mainBlockHeight() {
     itemMoreButton.classList.remove("hidden-button");
     itemMoreNum.textContent = excessButtons;
     const buttonsBannerHeight = bannerBlockJs.offsetHeight;
-    if (buttonsBannerHeight > 182) {
+    if (buttonsBannerHeight > size) {
       const buttons = bannerBlockJs.querySelectorAll(".buttons-banner__item:not(.item-more,.hidden-button)");
       buttons[buttons.length - 1].classList.add('hidden-button');
       itemMoreNum.textContent = excessButtons + 1;
--- src/js/components/blockHeightOfTitle.js
+++ src/js/components/blockHeightOfTitle.js
@@ -65,6 +65,11 @@ function mainBlockHeight() {
   const itemMoreNum = bannerBlockJs.querySelector(".link-info__date--num");
   const itemMoreButton = bannerBlockJs.querySelector(".item-more");
   let excessButtons = 0;
+  let size = 182
+
+  if (document.documentElement.clientWidth < 991) {
+    size = 80
+  }
 
   const buttons = bannerBlockJs.querySelectorAll(".buttons-banner__item:not(.item-more)");
   buttons.forEach(button => {
@@ -75,7 +80,7 @@ function mainBlockHeight() {
   [...buttons].reverse().forEach((button) => {
     const buttonsBannerHeight = bannerBlockJs.offsetHeight;
 
-    if (buttonsBannerHeight > 182) {
+    if (buttonsBannerHeight > size) {
       button.classList.add("hidden-button");
       excessButtons++;
     }
@@ -87,7 +92,7 @@ function mainBlockHeight() {
     itemMoreNum.textContent = excessButtons;
     const buttonsBannerHeight = bannerBlockJs.offsetHeight;
 
-    if (buttonsBannerHeight > 182) {
+    if (buttonsBannerHeight > size) {
       const buttons = bannerBlockJs.querySelectorAll(".buttons-banner__item:not(.item-more,.hidden-button)");
       buttons[buttons.length - 1].classList.add('hidden-button');
       itemMoreNum.textContent = excessButtons + 1;
